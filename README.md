64ba52eeb3c46cfcf7826d6b800e9cccb72d545d
Brian Ghidinelli <bghidinelli@hagerty.com>
2024-02-14 23:25:13 +0000

    Version 1.3 changes. [#186242032] Reearch bug report. Format code. Enhance shortcode options. Update styles. Replace register image with button. Add docker support for plugin development.

--- README.md
+++ README.md
@@ -22,6 +22,20 @@ There are two ways of embedding the MotorsportReg.com calendar in your Wordpress
   [msr_calendar]
   ```
 
+  You can also customize the settings per-instance (for example, if you wanted to have discipline-specific pages):
+
+  ```
+  [msr_calendar url="https://api.motorsportreg.com/rest/calendars/organization/{organization-id}?types={eventtype_id},{eventtype_id},{eventtype_id}"
+                title="My Second Calendar"
+                field_eventname=1
+                field_organization=0
+                field_venue=1
+                field_venuecity=1
+                field_eventtype=0
+                field_eventdate=1]
+  ```
+
+
 2. Use the PHP function in any theme template file: 
 
   For developers or hackers, you can also use the msr_calendar() function to embed the calendar into your theme with one simple function call.
@@ -41,4 +55,19 @@ There are two ways of embedding the MotorsportReg.com calendar in your Wordpress
   You can obtain your Organization-Id from your [admin here](https://www.motorsportreg.com/em360/index.cfm/event/profile.api).
   ![Install](http://api.motorsportreg.com/wordpress/admin.png)
 
-Tested as far back as Wordpress ~2.8 or so.  Should be compatible with the latest and greatest.  Please report here if you encounter issues: https://github.com/MotorsportReg/calendar-wordpress/issues 
+Tested as far back as Wordpress 2.8 and as new as 6.4. Should be compatible with the latest and greatest. Please report here if you encounter issues: https://github.com/MotorsportReg/calendar-wordpress/issues 
+
+
+## Plugin Development
+
+If you would like to hack on the plugin itself, you can run everything locally using Docker.
+
+Checkout the code and run: 
+
+`docker-compose up`
+
+We auto-restore a database backup that includes two pages with sample calendar plugin configurations.
+
+You can then access the running Wordpress site at http://localhost:84/
+
+You can login to the admin at http://localhost:84/wp-admin/ with `sysop@your-website-here.com` and password `LynhQVSM1Gyp%KZ51Z0KiVh5mvbN4oJlKgxH685Y#5pab@rCIJ@Be697%ZN`
--- msr-calendar/index.php
+++ msr-calendar/index.php
@@ -4,9 +4,9 @@
 Plugin Name: MotorsportReg.com Calendar
 Plugin URI: http://api.motorsportreg.com/wordpress/ 
 Description: Display upcoming events from MotorsportReg.com calendar
-Author: Pukka Software
-Version: 1.2
-Author URI: http://api.motorsportreg.com
+Author: MotorsportReg.com
+Version: 1.3
+Author URI: http://www.motorsportreg.com
 */
 
 // handle automatic updates without using wordpress.org
@@ -43,19 +43,25 @@ function motorsport_plugin_url( $path = '' ) {
 	return plugins_url( $path, MSR_CALENDAR_PLUGIN_BASENAME );
 }
 
+function reconstruct_url($url){
+    $url_parts = parse_url($url);
+	$constructed_url = $url_parts['scheme'] . '://' . $url_parts['host'] . (isset($url_parts['path'])?$url_parts['path']:'') . '?utm_source=' . urlencode($_SERVER['HTTP_HOST']) . '&amp;utm_medium=wordpress_plugin&amp;utm_campaign=motorsportreg';
+    return $constructed_url;
+}
+
 function msr_calendar_install() 
 {
 	global $wpdb;
 	
-		add_option('msr_calendar_title', "MotorsportReg.com Calendar");
-		add_option('msr_calendar_url',"");
-		add_option('msr_calendar_cache_time', "24");
-		add_option('msr_calendar_display_field_eventname', "1");
-		add_option('msr_calendar_display_field_organization', "1");
-		add_option('msr_calendar_display_field_venue', "1");
-		add_option('msr_calendar_display_field_venuecity',"1");
-		add_option('msr_calendar_display_field_eventtype',"1");
-		add_option('msr_calendar_display_field_eventdate',"1");        
+	add_option('msr_calendar_title', "MotorsportReg.com Calendar");
+	add_option('msr_calendar_url',"");
+	add_option('msr_calendar_cache_time', "24");
+	add_option('msr_calendar_display_field_eventname', "1");
+	add_option('msr_calendar_display_field_organization', "1");
+	add_option('msr_calendar_display_field_venue', "1");
+	add_option('msr_calendar_display_field_venuecity',"1");
+	add_option('msr_calendar_display_field_eventtype',"1");
+	add_option('msr_calendar_display_field_eventdate',"1");        
 	
 }
 
@@ -72,70 +78,70 @@ function msr_calendar_style(){
 
 function msr_calendar_control() 
 {
-	//echo "Motor Sports";
+	//echo "MotorsportReg";
 }
 
 
 
 function request_cache($url, $timeout, $flush = false) 
 {
-global $wpdb;
-$resource_url=$url;
-$expirey_time=$timeout;
-$flush_now=$flush;
-$key=generate_msr_unique_key($resource_url);
+	global $wpdb;
+	$resource_url=$url;
+	$expirey_time=$timeout;
+	$flush_now=$flush;
+	$key=generate_msr_unique_key($resource_url);
 
-if($flush_now){
-   delete_transient($key);
-}
-if ( false === ( $special_query_results = get_transient($key) ) || $flush_now === true)
- {
-   $data=fetch_data($url);
-   set_transient($key,$data,$expirey_time);
-    return $data;
- }else{
-    $special_query_results = get_transient($key);
-    return $special_query_results;
- }
+	if ($flush_now) {
+		delete_transient($key);
+	}
+	if (false === ( $special_query_results = get_transient($key) ) || $flush_now === true)
+	{
+		$data=fetch_data($url);
+		set_transient($key,$data,$expirey_time);
+		return $data;
+	} else {
+		$special_query_results = get_transient($key);
+		return $special_query_results;
+	}
 }
 
 
 function fetch_data($url)
 {
-     global $wpdb, $wp_version;
-                $ch = curl_init(); 
-                curl_setopt($ch, CURLOPT_URL, $url); 
-                curl_setopt ($ch,CURLOPT_FOLLOWLOCATION, 1); 
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-                curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30); 
-                curl_setopt($ch, CURLOPT_TIMEOUT, 90);
-                // set content-type and authorization headers
-                $headers = array(
-                        "Accept: application/vnd.pukkasoft+xml",
-                );
-                curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-                $data = curl_exec($ch);
-                $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-                curl_close($ch);
-                if($http_status == '200' )
-		        {
-                $data1=simplexml_load_string($data);
-                $arr = json_encode($data1);
-                return $arr;
-                }elseif($http_status == '404' || $http_status == '405' || $http_status == '415' || $http_status == '500')
-                {
-                    echo "No Record Found.  The URL may be incorrect.";
-                    exit;
-                }
-}
+    global $wpdb, $wp_version;
+	$ch = curl_init(); 
+	curl_setopt($ch, CURLOPT_URL, $url); 
+	curl_setopt ($ch,CURLOPT_FOLLOWLOCATION, 1); 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30); 
+	curl_setopt($ch, CURLOPT_TIMEOUT, 90);
 
+	// set content-type and authorization headers
+	$headers = array(
+		"Accept: application/vnd.pukkasoft+xml",
+	);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	$data = curl_exec($ch);
+	$http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	curl_close($ch);
+	if($http_status == '200' )
+	{
+		$data1=simplexml_load_string($data);
+		$arr = json_encode($data1);
+		return $arr;
+	} elseif ($http_status == '404' || $http_status == '405' || $http_status == '415' || $http_status == '500')
+	{
+		echo "No Record Found.  The URL may be incorrect.";
+		exit;
+	}
+}
 
 
 function generate_msr_unique_key($url){
      global $wpdb;
     $key=md5($url);
-return $key;
+	return $key;
 }
 
 function date_display_formate($start_date,$end_date='')
@@ -177,24 +183,31 @@ function registr_display_formate($start_date,$end_date)
     $current_date=strtotime(date('Y-m-d h:s'));
     if(($current_date >=$start) AND ($current_date<=$end) )
     {
-    $flag=true; 
+  	  $flag=true; 
     }else{
-    $flag=false;
+ 	   $flag=false;
     }
 
-return $flag;
+	return $flag;
 }
 
 function msr_calendar_shortcode($atts) 
 {
 	global $wpdb;
-        extract( shortcode_atts( array(
+	extract( shortcode_atts( array(
 		'url' => get_option('msr_calendar_url'),
-                ), $atts ) );
-        $cache_update_after_time=get_option('msr_calendar_cache_time') * 60 * 60;
-        $xml = request_cache($url,$cache_update_after_time);
-        $out=msr_calendar_view($xml);
-        return $out;
+		'title' => get_option('msr_calendar_title'),
+		'field_eventname' => get_option('msr_calendar_display_field_eventname'),
+		'field_organization' => get_option('msr_calendar_display_field_organization'),
+		'field_venue' => get_option('msr_calendar_display_field_venue'),
+		'field_venuecity' => get_option('msr_calendar_display_field_venuecity'),
+		'field_eventtype' => get_option('msr_calendar_display_field_eventtype'),
+		'field_eventdate' => get_option('msr_calendar_display_field_eventdate'),
+		), $atts ) );
+	$cache_update_after_time = get_option('msr_calendar_cache_time') * 60 * 60;
+	$xml = request_cache($url,$cache_update_after_time);
+	$out = msr_calendar_view($xml, $title, $field_eventname, $field_organization, $field_venue, $field_venuecity, $field_eventtype, $field_eventdate);
+	return $out;
 }
 function msr_calendar() 
 {
@@ -204,122 +217,117 @@ function msr_calendar()
 
 function msr_calendar_widget($args) 
 {
-               extract($args);
-		echo $before_widget;
-		echo $before_title;
-		echo get_option('msr_calendar_title');
-		echo $after_title;
-		msr_calendar();
-		echo $after_widget;
+    extract($args);
+	echo $before_widget;
+	echo $before_title;
+	echo get_option('msr_calendar_title');
+	echo $after_title;
+	msr_calendar();
+	echo $after_widget;
 }
 
 
-function msr_calendar_view($xml)
+function msr_calendar_view($xml, $title, $field_eventname, $field_organization, $field_venue, $field_venuecity, $field_eventtype, $field_eventdate)
 {
-$field_eventname=get_option('msr_calendar_display_field_eventname');
-$field_organization=get_option('msr_calendar_display_field_organization');
-$field_venue=get_option('msr_calendar_display_field_venue');
-$field_venuecity=get_option('msr_calendar_display_field_venuecity');
-$field_eventtype=get_option('msr_calendar_display_field_eventtype');
-$field_eventdate=get_option('msr_calendar_display_field_eventdate');   
-$title=get_option('msr_calendar_title');
-$data1 = json_decode($xml, true);
-$event_tot=$data1["recordset"]["total"];
-if ($event_tot == 0){
-$events= array();	
-}else if($event_tot == 1){
-$events= $data1["events"]; 
-}else{
-$events= $data1["events"]["event"]; 
-}
 
-$out = '<div class="msrcalendar"><h2>' . $title .'</h2>';
-$out .='<table><thead><tr>';
-$show=false;
+	$data1 = json_decode($xml, true);
+	$event_tot=$data1["recordset"]["total"];
+	if ($event_tot == 0){
+		$events= array();	
+	}else if($event_tot == 1){
+		$events= $data1["events"]; 
+	}else{
+		$events= $data1["events"]["event"]; 
+	}
 
-if($field_eventdate){
-	$out .='<th>Date</th>';
-	$show=true;
-}
+	$out = '<div class="msrcalendar"><h2>' . $title .'</h2>';
+	$out .='<table><thead><tr>';
+	$show=false;
 
-if($field_eventname){
-	$out .='<th>Name</th>';
-	$show=true;
-}
+	if($field_eventdate){
+		$out .='<th>Date</th>';
+		$show=true;
+	}
 
-if($field_organization){
-	$out .='<th>Organization</th>';
-	$show=true;
-}
+	if($field_eventname){
+		$out .='<th>Name</th>';
+		$show=true;
+	}
 
-if($field_venue){
-	$out .='<th>Venue</th>';
-	$show=true;
-}
+	if($field_organization){
+		$out .='<th>Organization</th>';
+		$show=true;
+	}
 
-if($field_venuecity){
-	$out .='<th>Location</th>';
-	$show=true;
-}
+	if($field_venue){
+		$out .='<th>Venue</th>';
+		$show=true;
+	}
 
-if($field_eventtype){
-	$out .='<th>Type</th>';
-	$show=true;
-}
+	if($field_venuecity){
+		$out .='<th>Location</th>';
+		$show=true;
+	}
 
-$out .='<th>&nbsp;</th>';
-$out .='</tr></thead><tbody>';
+	if($field_eventtype){
+		$out .='<th>Type</th>';
+		$show=true;
+	}
 
-if(count($events)){
+	$out .='<th>&nbsp;</th>';
+	$out .='</tr></thead><tbody>';
 
-	foreach($events as $event)
-	{
-		$out .='<tr>';
-	
-		if($field_eventdate){
-			$start=$event['start'];
-			$end=$event['end'];
-			if(!$end)
-				$out .='<td>'.date_display_formate($start).'</td>';
-			else
-				$out .='<td>'.date_display_formate($start,$end).'</td>';
-		}
-	
-		if($field_eventname)
-			$out .='<td>'.$event['name'].'</td>';
-	
-		if($field_organization)
-			$out .='<td>'.$event['organization']['name'].'</td>';
-	
-		if($field_venue)
-			$out .='<td>'.$event['venue']['name'].'</td>';
-	
-		if($field_venuecity)
-			$out .='<td>'.$event['venue']['city'].', '.$event['venue']['region'].'</td>';
-	
-		if($field_eventtype)
-			$out .='<td>'.$event['type'].'</td>';
-	
-		$r_start=$event['registration']['start'];
-		$r_end=$event['registration']['end'];
-		$flag=registr_display_formate($r_start,$r_end);
-		if($show){
-			if($flag)
-				$out .='<td><a href="'.$event['detailuri'].'" class="imglink"><img src="' . plugins_url('calendar/images/register.gif', __FILE__) . '" height="17" width="85" alt="Register now on MotorsportReg.com" /></a></td>';
-			else
-				$out .='<td><a href="'.$event['detailuri'].'" class="txtlink">More Details</a></td>';
-		}
+	if(count($events)){
 
-$out .='</tr>';
-}
-}else{
-	$out .='<tr><td colspan="7">No upcoming coming events for this organization.</td></tr>';
-}
+		foreach($events as $event)
+		{
+			$out .='<tr>';
+		
+			if($field_eventdate){
+				$start=$event['start'];
+				$end=$event['end'];
+				if(!$end)
+					$out .='<td>'.date_display_formate($start).'</td>';
+				else
+					$out .='<td>'.date_display_formate($start,$end).'</td>';
+			}
+		
+			if($field_eventname)
+				$out .='<td>'.$event['name'].'</td>';
+		
+			if($field_organization)
+				$out .='<td>'.$event['organization']['name'].'</td>';
+		
+			if($field_venue)
+				$out .='<td>'.$event['venue']['name'].'</td>';
+		
+			if($field_venuecity)
+				$out .='<td>'.$event['venue']['city'].', '.$event['venue']['region'].'</td>';
+		
+			if($field_eventtype)
+				$out .='<td>'.$event['type'].'</td>';
+		
+			$r_start=$event['registration']['start'];
+			$r_end=$event['registration']['end'];
+			$flag=registr_display_formate($r_start,$r_end);
+			if($show){
+				
+				if($flag)
+					$out .='<td><a href="' . reconstruct_url($event['detailuri']) . '" class="txtlink_register" alt="Register now on MotorsportReg.com">Register</a></td>';
+				else
+					$out .='<td><a href="' . reconstruct_url($event['detailuri']) . '" class="txtlink_details" alt="View event details on MotorsportReg.com">Details</a></td>';
+			}
 
-// please do not remove the link to motorsportreg.com - the only requirement for using our otherwise-free plugin is to retain an unedited link back to our site, thanks!
-$out .='</tbody></table><div class="morelink">Use MotorsportReg.com for <a href="http://www.motorsportreg.com/index.cfm/event/event-management">online driving event registration</a>.  Register for thousands of <a href="http://www.motorsportreg.com/calendar/">autocross, HPDE, race &amp; social events</a>.</div></div>';
+	$out .='</tr>';
+	}
+	}else{
+		$out .='<tr><td colspan="7">No upcoming coming events for this organization.</td></tr>';
+	}
 
-return $out;
+	// please do not remove the link to motorsportreg.com - the only requirement for using our otherwise-free plugin is to retain an unedited link back to our site, thanks!
+	$out .='</tbody></table><div class="morelink">Visit MotorsportReg.com to <a href="http://www.motorsportreg.com/calendar/">register for thousands of autocross, HPDE, race &amp; social events</a>.</div></div>';
+
+	return $out;
 }
 
 
